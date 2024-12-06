d6ecbae63209771f25fd5027618c4ffc572e4b01
kidanemriam <kide.fikadu1@gmail.com>
2024-12-06 15:30:35 +0300

    bug fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         IssueReport::observe(\App\Observers\IssueReportOberser::class);
-
-        URL::forceScheme('https');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -33,43 +33,11 @@
 </head>
 
 <body class="antialised">
-    <div class="flex flex-col h-screen justify-between">
-        <header>
-            <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="/" class="flex items-center">
-                        <img src="https://www.lersha.com/assets/vectors/logo_main.svg"
-                            class="mr-3 h-6 sm:h-9 block dark:hidden" alt="Lersha Logo" />
-                        <img src="https://www.lersha.com/assets/vectors/logo_main.svg"
-                            class="mr-3 h-6 sm:h-9 hidden dark:block" alt="Lersha Logo" />
-                        {{-- <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Lersha</span> --}}
-                    </a>
-                    <div class="flex items-center lg:order-2">
-                        
-                    </div>
-                </div>
-            </nav>
-        </header>
+    <div class="flex flex-col h-s
 
         <main class="mt-2 p-5 mb-auto">
             {{ $slot }}
         </main>
-
-        <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
-            <div class="mx-auto max-w-screen-xl text-center">
-                <a href="https://www.lersha.com/" target="blank"
-                    class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img src="https://www.lersha.com/assets/vectors/logo_main.svg"
-                        class="mr-3 h-6 sm:h-9 block dark:hidden" alt="Lersha Logo" />
-                    <img src="https://www.lersha.com/assets/vectors/logo_main.svg"
-                        class="mr-3 h-6 sm:h-9 hidden dark:block" alt="Lersha Logo" />
-                    {{-- <span class="self-center">Lersha</span>     --}}
-                </a>
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a
-                        href="#" class="hover:underline">Lersha™</a>. All Rights Reserved.</span>
-            </div>
-        </footer>
-        @livewire('wire-elements-modal')
     </div>
 </body>
 
