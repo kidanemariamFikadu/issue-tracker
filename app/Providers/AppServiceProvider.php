aba9e035d88b966920948e507f5a79806774833a
kidanemriam <kide.fikadu1@gmail.com>
2024-12-06 11:56:40 +0300

    fixing secure asset issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\IssueReport;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         IssueReport::observe(\App\Observers\IssueReportOberser::class);
+
+        URL::forceScheme('https');
     }
 }
