d97e084925ba99014a07c3e814aaeed2315ea416
kidanemriam <kide.fikadu1@gmail.com>
2024-12-07 08:29:15 +0300

    moving issues to the right and securing logout button

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -72,7 +72,7 @@
                                 {{-- <div class="border-t border-gray-200"></div> --}}
                             </ul>
                             <div class="py-1">
-                                <form method="POST" action="{{ route('logout') }}" x-data>
+                                <form method="POST" action="{{ secure_url(route('logout', [], false)) }}" x-data>
                                     @csrf
                                     <a href="{{ route('logout') }}" @click.prevent="$root.submit();"
                                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
@@ -80,6 +80,8 @@
                                 </form>
                             </div>
                         </div>
+                        <a href="/issues"
+                                    class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Issues</a>
                         @role('admin')
                             <a href="/settings"
                                 class="text-gray-700 dark:text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
