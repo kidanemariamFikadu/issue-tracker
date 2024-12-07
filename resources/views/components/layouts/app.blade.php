e4819aa4f2a77eed77b3f533c2216f139be7ef0f
kidanemriam <kide.fikadu1@gmail.com>
2024-12-07 08:31:54 +0300

    ui fix: add missing semicolon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -44,6 +44,16 @@
                             class="mr-3 h-6 sm:h-9 hidden dark:block" alt="Lersha Logo" />
                         {{-- <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Lersha</span> --}}
                     </a>
+                    
+                    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-2"
+                        id="mobile-menu-2">
+                        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <a href="/issues"
+                                    class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Issues</a>
+                            </li>
+                        </ul>
+                    </div>
                     <div class="flex items-center lg:order-2">
                         <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
                             class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
@@ -103,15 +113,6 @@
                                     clip-rule="evenodd"></path>
                             </svg>
                         </button>
-                    </div>
-                    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-2"
-                        id="mobile-menu-2">
-                        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <a href="/issues"
-                                    class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Issues</a>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </nav>
