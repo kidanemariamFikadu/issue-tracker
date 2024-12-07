29930eab68175840f74e6804482cb757c16f2eb6
kidanemriam <kide.fikadu1@gmail.com>
2024-12-07 08:30:55 +0300

    bug fix

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -80,8 +80,6 @@
                                 </form>
                             </div>
                         </div>
-                        <a href="/issues"
-                                    class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Issues</a>
                         @role('admin')
                             <a href="/settings"
                                 class="text-gray-700 dark:text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
@@ -106,19 +104,13 @@
                             </svg>
                         </button>
                     </div>
-                    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-2"
                         id="mobile-menu-2">
                         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            {{-- <li>
-                                <a href="/"
-                                    class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                                    aria-current="page">Home</a>
-                            </li> --}}
                             <li>
                                 <a href="/issues"
                                     class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Issues</a>
                             </li>
-
                         </ul>
                     </div>
                 </div>
