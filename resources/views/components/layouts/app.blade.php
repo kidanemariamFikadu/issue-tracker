b3519d367f296f295d641262bf8255ddc556fc1d
kidanemriam <kide.fikadu1@gmail.com>
2024-12-08 13:05:06 +0300

    notification fix

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -131,7 +131,7 @@
     <script>
         // Listen for the Livewire 'show-toast' event
         Livewire.on('show-toast', (data) => {
-            console.log(data)
+            console.log(data[0])
             const container = document.getElementById('toast-container');
 
             // Determine type-specific styles and icons
@@ -165,7 +165,7 @@
                 }
             };
 
-            const toastType = types[data.type] || types.info; // Default to 'info' if type not found
+            const toastType = types[data[0].type] || types.info; // Default to 'info' if type not found
 
             // Create a new toast element
             const toast = document.createElement('div');
@@ -175,7 +175,7 @@
                 <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg ${toastType.iconBg}">
                     ${toastType.icon}
                 </div>
-                <div class="ml-3 text-sm font-normal">${data.message}</div>
+                <div class="ml-3 text-sm font-normal">${data[0].message}</div>
                 <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" aria-label="Close" onclick="this.parentElement.remove();">
                     <span class="sr-only">Close</span>
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="https://www.w3.org/2000/svg">
