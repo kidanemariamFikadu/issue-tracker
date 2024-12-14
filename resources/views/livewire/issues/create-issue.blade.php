0c61cbc9da602184bc3936be5e4bb9ae4e8fee26
kidanemriam <kide.fikadu1@gmail.com>
2024-12-14 11:50:31 +0300

    spelling error correction

--- resources/views/livewire/issues/create-issue.blade.php
+++ resources/views/livewire/issues/create-issue.blade.php
@@ -47,7 +47,7 @@
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Agent</label>
                     <select id="agent" wire:model='agentId'
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                        <option selected>Choose an agent (optianal)</option>
+                        <option selected>Choose an agent (optional)</option>
                         @foreach ($agents as $agent)
                             <option value="{{ $agent->id }}">
                                 {{ $agent->first_name . ' ' . $agent->last_name . ' (' . $agent->phone . ')' }}</option>
