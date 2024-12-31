9c530bb3c465d76746b5b89bd489d6588f442cf5
kidanemriam <kide.fikadu1@gmail.com>
2024-12-31 16:17:30 +0300

    fixed bug issues

--- app/Livewire/Report/GeneralReport.php
+++ app/Livewire/Report/GeneralReport.php
@@ -21,7 +21,7 @@ class GeneralReport extends Component
 
     public function mount()
     {
-        $this->weekDate = \Carbon\Carbon::now();
+        // $this->weekDate = \Carbon\Carbon::now();
         $this->issuesPerPeriod();
     }
 
@@ -34,81 +34,80 @@ class GeneralReport extends Component
     private function issuesPerPeriod()
     {
         $query = IssueReport::query();
+        $selectPeriod = '';
+        $groupBy = '';
 
-        // if ($this->period === 'day') {
-        //     $query->whereBetween('created_at', [$start->startOfDay(), $end->endOfDay()]);
-        //     $groupBy = 'HOUR(created_at)';
-        //     $selectPeriod = "strftime('%Y-%m-%d %H:00:00', created_at) as period";
-        // } else
-        if ($this->period === 'week') {
-            $start = $this->weekDate->copy()->startOfWeek();
-            $end = $this->weekDate->copy()->endOfWeek();
-            $query->whereBetween('created_at', [$start->startOfWeek(), $end->endOfWeek()]);
-            $selectPeriod = "
-        CASE strftime('%w', created_at)
-            WHEN '0' THEN 'Sunday'
-            WHEN '1' THEN 'Monday'
-            WHEN '2' THEN 'Tuesday'
-            WHEN '3' THEN 'Wednesday'
-            WHEN '4' THEN 'Thursday'
-            WHEN '5' THEN 'Friday'
-            WHEN '6' THEN 'Saturday'
-        END || ', ' || 
-        CASE strftime('%m', created_at)
-            WHEN '01' THEN 'January'
-            WHEN '02' THEN 'February'
-            WHEN '03' THEN 'March'
-            WHEN '04' THEN 'April'
-            WHEN '05' THEN 'May'
-            WHEN '06' THEN 'June'
-            WHEN '07' THEN 'July'
-            WHEN '08' THEN 'August'
-            WHEN '09' THEN 'September'
-            WHEN '10' THEN 'October'
-            WHEN '11' THEN 'November'
-            WHEN '12' THEN 'December'
-        END || ' ' || strftime('%Y', created_at) AS period";
-        } elseif ($this->period === 'month') {
-            $start = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->startOfMonth();
-            $end = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->endOfMonth();
-            $query->whereBetween('created_at', [$start, $end]);
-            $groupBy = "strftime('%d', created_at)";
-            $selectPeriod = "strftime('%d', created_at)
-            || ', ' || 
-        CASE strftime('%m', created_at)
-            WHEN '01' THEN 'January'
-            WHEN '02' THEN 'February'
-            WHEN '03' THEN 'March'
-            WHEN '04' THEN 'April'
-            WHEN '05' THEN 'May'
-            WHEN '06' THEN 'June'
-            WHEN '07' THEN 'July'
-            WHEN '08' THEN 'August'
-            WHEN '09' THEN 'September'
-            WHEN '10' THEN 'October'
-            WHEN '11' THEN 'November'
-            WHEN '12' THEN 'December'
-        END || ' ' || strftime('%Y', created_at) AS period";
-        } elseif ($this->period === 'year') {
-            $start = \Carbon\Carbon::createFromDate($this->year, 1, 1)->startOfYear();
-            $end = \Carbon\Carbon::createFromDate($this->year, 1, 1)->endOfYear();
-            $query->whereBetween('created_at', [$start, $end]);
-            $groupBy = 'MONTH(created_at)';
-            $selectPeriod = "CASE strftime('%m', created_at)
-            WHEN '01' THEN 'January'
-            WHEN '02' THEN 'February'
-            WHEN '03' THEN 'March'
-            WHEN '04' THEN 'April'
-            WHEN '05' THEN 'May'
-            WHEN '06' THEN 'June'
-            WHEN '07' THEN 'July'
-            WHEN '08' THEN 'August'
-            WHEN '09' THEN 'September'
-            WHEN '10' THEN 'October'
-            WHEN '11' THEN 'November'
-            WHEN '12' THEN 'December'
-            END ||', '|| strftime('%Y', created_at) AS period";
+        switch ($this->period) {
+            case 'week':
+                $start = \Carbon\Carbon::parse($this->weekDate)->startOfWeek();
+                $end = \Carbon\Carbon::parse($this->weekDate)->endOfWeek();
+                $selectPeriod = "
+                    CASE strftime('%w', created_at)
+                        WHEN '0' THEN 'Sunday'
+                        WHEN '1' THEN 'Monday'
+                        WHEN '2' THEN 'Tuesday'
+                        WHEN '3' THEN 'Wednesday'
+                        WHEN '4' THEN 'Thursday'
+                        WHEN '5' THEN 'Friday'
+                        WHEN '6' THEN 'Saturday'
+                    END || ', ' || 
+                    CASE strftime('%m', created_at)
+                        WHEN '01' THEN 'January'
+                        WHEN '02' THEN 'February'
+                        WHEN '03' THEN 'March'
+                        WHEN '04' THEN 'April'
+                        WHEN '05' THEN 'May'
+                        WHEN '06' THEN 'June'
+                        WHEN '07' THEN 'July'
+                        WHEN '08' THEN 'August'
+                        WHEN '09' THEN 'September'
+                        WHEN '10' THEN 'October'
+                        WHEN '11' THEN 'November'
+                        WHEN '12' THEN 'December'
+                    END || ' ' || strftime('%Y', created_at) AS period";
+                break;
+            case 'month':
+                $start = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->startOfMonth();
+                $end = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->endOfMonth();
+                $groupBy = "strftime('%d', created_at)";
+                $selectPeriod = "strftime('%d', created_at) || ', ' || 
+                    CASE strftime('%m', created_at)
+                        WHEN '01' THEN 'January'
+                        WHEN '02' THEN 'February'
+                        WHEN '03' THEN 'March'
+                        WHEN '04' THEN 'April'
+                        WHEN '05' THEN 'May'
+                        WHEN '06' THEN 'June'
+                        WHEN '07' THEN 'July'
+                        WHEN '08' THEN 'August'
+                        WHEN '09' THEN 'September'
+                        WHEN '10' THEN 'October'
+                        WHEN '11' THEN 'November'
+                        WHEN '12' THEN 'December'
+                    END || ' ' || strftime('%Y', created_at) AS period";
+                break;
+            case 'year':
+                $start = \Carbon\Carbon::createFromDate($this->year, 1, 1)->startOfYear();
+                $end = \Carbon\Carbon::createFromDate($this->year, 1, 1)->endOfYear();
+                $groupBy = 'strftime(\'%m\', created_at)';
+                $selectPeriod = "CASE strftime('%m', created_at)
+                    WHEN '01' THEN 'January'
+                    WHEN '02' THEN 'February'
+                    WHEN '03' THEN 'March'
+                    WHEN '04' THEN 'April'
+                    WHEN '05' THEN 'May'
+                    WHEN '06' THEN 'June'
+                    WHEN '07' THEN 'July'
+                    WHEN '08' THEN 'August'
+                    WHEN '09' THEN 'September'
+                    WHEN '10' THEN 'October'
+                    WHEN '11' THEN 'November'
+                    WHEN '12' THEN 'December'
+                END || ', ' || strftime('%Y', created_at) AS period";
+                break;
         }
+
+        $query->whereBetween('created_at', [$start, $end]);
 
         if ($this->application) {
             $query->where('application_id', $this->application);
--- app/Livewire/Report/UserReport.php
+++ app/Livewire/Report/UserReport.php
@@ -21,7 +21,7 @@ class UserReport extends Component
 
     public function mount()
     {
-        $this->weekDate = \Carbon\Carbon::now();
+        // $this->weekDate = \Carbon\Carbon::now();
         $this->issuesPerPeriod();
     }
 
@@ -38,78 +38,86 @@ class UserReport extends Component
         }
 
         $query = IssueReport::query();
+        $start = null;
+        $end = null;
+        $groupBy = '';
+        $selectPeriod = '';
 
-        if ($this->period === 'week') {
-            $start = $this->weekDate->copy()->startOfWeek();
-            $end = $this->weekDate->copy()->endOfWeek();
-            $query->whereBetween('created_at', [$start->startOfWeek(), $end->endOfWeek()]);
-            $groupBy = "strftime('%d', created_at)";
-            $selectPeriod = "CASE strftime('%w', created_at)
-            WHEN '0' THEN 'Sunday'
-            WHEN '1' THEN 'Monday'
-            WHEN '2' THEN 'Tuesday'
-            WHEN '3' THEN 'Wednesday'
-            WHEN '4' THEN 'Thursday'
-            WHEN '5' THEN 'Friday'
-            WHEN '6' THEN 'Saturday'
-        END || ', ' || 
-        CASE strftime('%m', created_at)
-            WHEN '01' THEN 'January'
-            WHEN '02' THEN 'February'
-            WHEN '03' THEN 'March'
-            WHEN '04' THEN 'April'
-            WHEN '05' THEN 'May'
-            WHEN '06' THEN 'June'
-            WHEN '07' THEN 'July'
-            WHEN '08' THEN 'August'
-            WHEN '09' THEN 'September'
-            WHEN '10' THEN 'October'
-            WHEN '11' THEN 'November'
-            WHEN '12' THEN 'December'
-        END || ' ' || strftime('%Y', created_at) AS period";
-        } elseif ($this->period === 'month') {
-            $start = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->startOfMonth();
-            $end = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->endOfMonth();
-            $query->whereBetween('created_at', [$start, $end]);
-            $groupBy = "strftime('%d', created_at)";
-            $selectPeriod = "strftime('%d', created_at)
-            || ', ' || 
-        CASE strftime('%m', created_at)
-            WHEN '01' THEN 'January'
-            WHEN '02' THEN 'February'
-            WHEN '03' THEN 'March'
-            WHEN '04' THEN 'April'
-            WHEN '05' THEN 'May'
-            WHEN '06' THEN 'June'
-            WHEN '07' THEN 'July'
-            WHEN '08' THEN 'August'
-            WHEN '09' THEN 'September'
-            WHEN '10' THEN 'October'
-            WHEN '11' THEN 'November'
-            WHEN '12' THEN 'December'
-        END || ' ' || strftime('%Y', created_at) AS period";
-        } elseif ($this->period === 'year') {
-            $start = \Carbon\Carbon::createFromDate($this->year, 1, 1)->startOfYear();
-            $end = \Carbon\Carbon::createFromDate($this->year, 1, 1)->endOfYear();
-            $query->whereBetween('created_at', [$start, $end]);
-            $groupBy = 'MONTH(created_at)';
-            $selectPeriod = "CASE strftime('%m', created_at)
-            WHEN '01' THEN 'January'
-            WHEN '02' THEN 'February'
-            WHEN '03' THEN 'March'
-            WHEN '04' THEN 'April'
-            WHEN '05' THEN 'May'
-            WHEN '06' THEN 'June'
-            WHEN '07' THEN 'July'
-            WHEN '08' THEN 'August'
-            WHEN '09' THEN 'September'
-            WHEN '10' THEN 'October'
-            WHEN '11' THEN 'November'
-            WHEN '12' THEN 'December'
-            END ||', '|| strftime('%Y', created_at) AS period";
+        switch ($this->period) {
+            case 'week':
+                $start = \Carbon\Carbon::parse($this->weekDate)->startOfWeek();
+                $end = \Carbon\Carbon::parse($this->weekDate)->endOfWeek();
+                $groupBy = "strftime('%d', created_at)";
+                $selectPeriod = "CASE strftime('%w', created_at)
+                    WHEN '0' THEN 'Sunday'
+                    WHEN '1' THEN 'Monday'
+                    WHEN '2' THEN 'Tuesday'
+                    WHEN '3' THEN 'Wednesday'
+                    WHEN '4' THEN 'Thursday'
+                    WHEN '5' THEN 'Friday'
+                    WHEN '6' THEN 'Saturday'
+                END || ', ' || 
+                CASE strftime('%m', created_at)
+                    WHEN '01' THEN 'January'
+                    WHEN '02' THEN 'February'
+                    WHEN '03' THEN 'March'
+                    WHEN '04' THEN 'April'
+                    WHEN '05' THEN 'May'
+                    WHEN '06' THEN 'June'
+                    WHEN '07' THEN 'July'
+                    WHEN '08' THEN 'August'
+                    WHEN '09' THEN 'September'
+                    WHEN '10' THEN 'October'
+                    WHEN '11' THEN 'November'
+                    WHEN '12' THEN 'December'
+                END || ' ' || strftime('%Y', created_at) AS period";
+                break;
+
+            case 'month':
+                $start = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->startOfMonth();
+                $end = \Carbon\Carbon::createFromDate($this->monthYear, $this->month, 1)->endOfMonth();
+                $groupBy = "strftime('%d', created_at)";
+                $selectPeriod = "strftime('%d', created_at)
+                || ', ' || 
+                CASE strftime('%m', created_at)
+                    WHEN '01' THEN 'January'
+                    WHEN '02' THEN 'February'
+                    WHEN '03' THEN 'March'
+                    WHEN '04' THEN 'April'
+                    WHEN '05' THEN 'May'
+                    WHEN '06' THEN 'June'
+                    WHEN '07' THEN 'July'
+                    WHEN '08' THEN 'August'
+                    WHEN '09' THEN 'September'
+                    WHEN '10' THEN 'October'
+                    WHEN '11' THEN 'November'
+                    WHEN '12' THEN 'December'
+                END || ' ' || strftime('%Y', created_at) AS period";
+                break;
+
+            case 'year':
+                $start = \Carbon\Carbon::createFromDate($this->year, 1, 1)->startOfYear();
+                $end = \Carbon\Carbon::createFromDate($this->year, 1, 1)->endOfYear();
+                $groupBy = 'MONTH(created_at)';
+                $selectPeriod = "CASE strftime('%m', created_at)
+                    WHEN '01' THEN 'January'
+                    WHEN '02' THEN 'February'
+                    WHEN '03' THEN 'March'
+                    WHEN '04' THEN 'April'
+                    WHEN '05' THEN 'May'
+                    WHEN '06' THEN 'June'
+                    WHEN '07' THEN 'July'
+                    WHEN '08' THEN 'August'
+                    WHEN '09' THEN 'September'
+                    WHEN '10' THEN 'October'
+                    WHEN '11' THEN 'November'
+                    WHEN '12' THEN 'December'
+                END ||', '|| strftime('%Y', created_at) AS period";
+                break;
         }
 
-        $query->where('assigned_to', $this->user);
+        $query->whereBetween('created_at', [$start, $end])
+              ->where('assigned_to', $this->user);
 
         $this->reportData = $query->selectRaw('COUNT(*) as total, 
               SUM(status = "Closed") as closed, 
