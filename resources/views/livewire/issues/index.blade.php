910606d7bd38ece9a1c4f3375a587f25e2095f2f
kidanemriam <kide.fikadu1@gmail.com>
2025-01-01 11:50:58 +0300

    bug fix

--- resources/views/livewire/issues/index.blade.php
+++ resources/views/livewire/issues/index.blade.php
@@ -74,7 +74,7 @@
 
                             @role('admin')
                                 <div class="flex flex-col">
-                                    <button wire:click="toggleMyIssues"
+                                    <button wire:click="exportIssues"
                                         class="px-3 py-1 rounded bg-gray-200 text-black' }}">
                                         <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                             width="24" height="24" fill="none" viewBox="0 0 24 24">
