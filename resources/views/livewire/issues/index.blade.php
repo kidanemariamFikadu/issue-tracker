1e1fde2ee31183890686d94e84ce2ab8bbf25401
kidanemriam <kide.fikadu1@gmail.com>
2025-01-14 13:28:37 +0300

    added word wrapping and overflow for issue description

--- resources/views/livewire/issues/index.blade.php
+++ resources/views/livewire/issues/index.blade.php
@@ -128,13 +128,9 @@
                         <tbody>
                             @foreach ($this->issueList as $issue)
                                 <tr wire:key="{{ $issue->id }}" class="border-b dark:border-gray-700">
-                                    {{-- <td>
-                                        <input type="checkbox" wire:model="selectedStudents"
-                                            value="{{ $student->id }}">
-                                    </td> --}}
                                     <th scope="row"
-                                        class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {{ $issue->issue }}</th>
+                                        class="px-4 py-3 font-medium text-gray-900 whitespace-normal dark:text-white">
+                                        {{ Str::limit($issue->issue, 50, '...') }}</th>
                                     <td class="px-4 py-3">{{ $issue->application?->name }}</td>
                                     <td class="px-4 py-3">{{ $issue->category?->name }}</td>
                                     <td class="px-4 py-3">
